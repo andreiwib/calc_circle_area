a9438a76d675338a231ac68b6a07e791a1535a0d
andreiwib <93547136+andreiwib@users.noreply.github.com>
2022-03-13 21:36:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var x=Math.PI;
+var r=prompt("Introdu valoarea razei pentru a calcula aria cercului!");
+var aria="Aria cercului pentru raza introdusa este: " + x*Math.pow(r,2);
+document.write(aria);
